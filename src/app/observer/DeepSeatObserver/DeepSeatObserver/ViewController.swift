ab23dbf56eb175ab9cea547be955e6434f575ce0
soc06212 <soc06212@gmail.com>
2022-03-05 09:29:06 +0900

    doc: gitignore 추가 및 프로젝트 추가

---
+++ src/app/observer/DeepSeatObserver/DeepSeatObserver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeepSeatObserver
+//
+//  Created by 최명근 on 2022/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
