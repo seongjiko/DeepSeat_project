4539254e9cae92f5785d37b199e9acd38d4a51dc
soc06212 <soc06212@gmail.com>
2022-03-23 09:19:31 +0900

    Feat: Spring Boot 프로젝트 생성

---
+++ src/server/DeepSeatServer/src/test/kotlin/com/deepseat/server/DeepSeatServer/DeepSeatServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deepseat.server.DeepSeatServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeepSeatServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
