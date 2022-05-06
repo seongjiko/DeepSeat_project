5103a48d8656824bb3e0de09dfd6cfbe0029ed0b
soc06212 <soc06212@gmail.com>
2022-05-06 20:33:39 +0900

    Feat: Mybatis 설정

---
+++ src/server/DeepSeatServer/src/main/kotlin/com/deepseat/server/DeepSeatServer/config/MybatisConfig.kt
@@ -0,0 +1,34 @@
+package com.deepseat.server.DeepSeatServer.config
+
+import org.apache.ibatis.session.SqlSessionFactory
+import org.mybatis.spring.SqlSessionFactoryBean
+import org.mybatis.spring.SqlSessionTemplate
+import org.mybatis.spring.annotation.MapperScan
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+
+@Configuration
+@MapperScan(basePackages = ["com.deepseat.server.DeepSeatServer.dao"])
+class MybatisConfig {
+    @Value("\${spring.datasource.mapper-locations}")
+    var path: String? = null
+
+    @Bean
+    @Throws(Exception::class)
+    fun sqlSessionFactory(dataSource: DataSource?, context: ApplicationContext): SqlSessionFactory? {
+        val sqlSessionFactory = SqlSessionFactoryBean()
+        sqlSessionFactory.setDataSource(dataSource)
+        sqlSessionFactory.setTypeAliasesPackage("com.deepseat.server.DeepSeatServer.vo")
+        sqlSessionFactory.setMapperLocations(*context.getResources(path!!))
+        return sqlSessionFactory.getObject()
+    }
+
+    @Bean
+    fun sqlSession(sqlSessionFactory: SqlSessionFactory?): SqlSessionTemplate {
+        return SqlSessionTemplate(sqlSessionFactory)
+    }
+}
