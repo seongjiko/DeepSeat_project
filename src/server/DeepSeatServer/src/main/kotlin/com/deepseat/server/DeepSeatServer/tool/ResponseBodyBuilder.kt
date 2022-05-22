9e0bc54cc015f36231652fd5f91e15a0ddced3c5
soc06212 <soc06212@gmail.com>
2022-05-22 14:38:06 +0900

    Feat: ResponseBody 구조 변경

--- src/server/DeepSeatServer/src/main/kotlin/com/deepseat/server/DeepSeatServer/tool/ResponseBodyBuilder.kt
+++ src/server/DeepSeatServer/src/main/kotlin/com/deepseat/server/DeepSeatServer/tool/ResponseBodyBuilder.kt
@@ -3,86 +3,59 @@ package com.deepseat.server.DeepSeatServer.tool
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 
-class ResponseBody {
-    companion object {
-        val RESPONSE_CODE = "responseCode"
-        val MESSAGE = "message"
-        val TOTAL_CNT = "totalCnt"
-        val HAS_MORE = "hasMore"
-        val PAGE = "page"
-        val DATA = "data"
-    }
-}
+class ResponseBody<T>(
+    var responseCode: Int = 200,
+    var message: String = "Success",
+    var totalCnt: Int? = null,
+    var hasMore: Boolean? = null,
+    var page: Int? = null,
+    var data: T? = null
+)
 
 class ResponseBodyBuilder<T> {
 
-    private var responseCode: Int = 200
-    private var message: String = "Success"
-    private var totalCnt: Int? = null
-    private var hasMore: Boolean? = null
-    private var page: Int? = null
-    private var data: T? = null
+    private var responseBody = ResponseBody<T>()
 
     constructor()
 
     constructor(error: com.deepseat.server.DeepSeatServer.error.Error) : this(error.errorCode, error.message)
 
     constructor(responseCode: Int, message: String) {
-        this.responseCode = responseCode
-        this.message = message
+        responseBody.responseCode = responseCode
+        responseBody.message = message
     }
 
     fun responseCode(responseCode: Int): ResponseBodyBuilder<T> {
-        this.responseCode = responseCode
+        responseBody.responseCode = responseCode
         return this
     }
 
     fun message(message: String): ResponseBodyBuilder<T> {
-        this.message = message
+        responseBody.message = message
         return this
     }
 
     fun totalCnt(totalCount: Int): ResponseBodyBuilder<T> {
-        this.totalCnt = totalCount
+        responseBody.totalCnt = totalCount
         return this
     }
 
     fun hasMore(hasMore: Boolean = true): ResponseBodyBuilder<T> {
-        this.hasMore = hasMore
+        responseBody.hasMore = hasMore
         return this
     }
 
     fun page(page: Int): ResponseBodyBuilder<T> {
-        this.page = page
+        responseBody.page = page
         return this
     }
 
     fun data(data: T): ResponseBodyBuilder<T> {
-        this.data = data
+        responseBody.data = data
         return this
     }
 
     override fun toString(): String {
-        val json = JsonObject()
-        json.addProperty(ResponseBody.RESPONSE_CODE, this.responseCode)
-        json.addProperty(ResponseBody.MESSAGE, this.message)
-
-        this.totalCnt?.let {
-            json.addProperty(ResponseBody.TOTAL_CNT, it)
-        }
-
-        this.hasMore?.let {
-            json.addProperty(ResponseBody.HAS_MORE, it)
-        }
-
-        this.page?.let {
-            json.addProperty(ResponseBody.PAGE, it)
-        }
-
-        this.data?.let {
-            json.add(ResponseBody.DATA, Gson().toJsonTree(it))
-        }
-
-        return json.toString()
+        return Gson().toJson(responseBody)
     }
 }
