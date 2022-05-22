f984494df2d17f391a8543763d51ed2bae00e7ab
soc06212 <soc06212@gmail.com>
2022-05-22 11:41:44 +0900

    Feat: Android 12 Splash Screen 지원

--- src/app/android/app/src/main/java/com/deepseat/ds/GlobalData.kt
+++ src/app/android/app/src/main/java/com/deepseat/ds/GlobalData.kt
@@ -5,7 +5,7 @@ import com.deepseat.ds.model.User
 class GlobalData {
 
     companion object {
-        val user: User = User("soc06212@gmail.com", "Super Genius MG")
+        val user: User? = null
     }
 
 }
--- src/app/android/app/src/main/java/com/deepseat/ds/MainActivity.kt
+++ src/app/android/app/src/main/java/com/deepseat/ds/MainActivity.kt
@@ -3,12 +3,16 @@ package com.deepseat.ds
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.deepseat.ds.databinding.ActivityMainBinding
 import com.deepseat.ds.fragment.CommunityFragment
 import com.deepseat.ds.fragment.MoreFragment
 import com.deepseat.ds.fragment.SeatsFragment
 import com.google.android.material.navigation.NavigationBarView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
@@ -18,7 +22,14 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
     private lateinit var binding: ActivityMainBinding
 
+    private var logInReady: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Android Splash Screen (Since 12)
+        installSplashScreen()
+        initSplashScreen()
+
+        // View Initializations
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,6 +39,31 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         // Bottom Nav View
         binding.bnvMain.setOnItemSelectedListener(this)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(timeMillis = 1000)
+            logInReady = true
+        }
+    }
+
+    private fun initSplashScreen() {
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (logInReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
