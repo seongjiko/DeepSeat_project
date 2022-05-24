3a829036eb665b70c396a5e240cabc4ffc56e55d
soc06212 <soc06212@gmail.com>
2022-05-24 15:36:50 +0900

    Fix: seat 정보 수정 안되는 문제 수정

--- src/server/DeepSeatServer/src/main/kotlin/com/deepseat/server/DeepSeatServer/DeepSeatServerApplication.kt
+++ src/server/DeepSeatServer/src/main/kotlin/com/deepseat/server/DeepSeatServer/DeepSeatServerApplication.kt
@@ -1,11 +1,37 @@
 package com.deepseat.server.DeepSeatServer
 
+import org.apache.catalina.connector.Connector
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.context.annotation.Configuration
+
 
 @SpringBootApplication
 class DeepSeatServerApplication
 
 fun main(args: Array<String>) {
 	runApplication<DeepSeatServerApplication>(*args)
+}
+
+@Configuration
+class TomcatWebServerCustomizer : WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+	/**
+	 * 톰캣에 옵션 추가.
+	 *
+	 * @param factory
+	 */
+	override fun customize(factory: TomcatServletWebServerFactory) {
+		factory.addConnectorCustomizers(TomcatConnectorCustomizer { connector: Connector ->
+			connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}")
+//			connector.setAttribute(
+//				"relaxedQueryChars",
+//				"<>[\\]^`{|}"
+//			)
+		})
+	}
 }
